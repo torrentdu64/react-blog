97b8b547dbcb2deb5eb00e1c7b73842055634eaf
torrent benoit <torrentdu64@gmail.com>
2017-09-02 14:19:24 +0200

    validation form with touched and error

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -4,25 +4,33 @@ import {Field, reduxForm} from 'redux-form';
 
 class PostsNew extends Component {
   renderField(field){
+    const { meta: {touched, error} } = field;
+    const className = `form-group ${ touched && error ? 'has-danger' : '' }`;
     return (
-      <div className="form-group">
+      <div className={className}>
       <label>{field.label}</label>
         <input
         className="form-control"
         type="text"
         {...field.input}
         />
+        <div className="text-help">
+        { touched ? error : ''}
+        </div>
       </div>
       );
 
   }
 
-
+  onSubmit(values){
+      console.log(values);
+  }
 
 
   render(){
+   const {handleSubmit} = this.props;
     return (
-      <form>
+      <form onSubmit={ handleSubmit(this.onSubmit.bind(this))}>
         <Field
         label="Title"
         name="title"
@@ -38,6 +46,7 @@ class PostsNew extends Component {
         name="content"
         component={this.renderField}
         />
+        <button type="submit" className="btn btn-primary">Submit</button>
       </form>
       );
   }
